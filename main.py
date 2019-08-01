7a20739cd5df39f7d439f07f05dab342d6e8e78e
Lucas Bäckvall <lucasba@student.chalmers.se>
2019-08-01 19:04:02 +0200

    Added GUI to unfinished game.

--- main.py
+++ main.py
@@ -1,84 +1,49 @@
-import random
-import sys
+import pygame
+import roj
 
-class roj(object):
-    
-    board = []
-    revealed = []
-    width = 20
-    height = 10
-    
-    def __init__(self, width=20, height=10):
-        self.width = width
-        self.height = height
-        for y in range(self.height):
-            self.board.append([])
-            self.revealed.append([])
-            for x in range(self.width):
-                self.board[y].append(random.choice(["*", " ", " ", " ", " ", " ", " "]))
-                self.revealed[y].append(False)
-        self.countBoard()
-    
-    def showBoard(self, showAll=False):
-        print("\n  ", end="")
-        for x in range(self.width):
-            print(str(x%10), end="")
-        print("")
-        for y in range(self.height):
-            print(str(y%10), end=" ")
-            for x in range(self.width):
-                if not self.revealed[y][x] and not showAll:
-                    print("X", end="")
-                elif self.board[y][x] == "0":
-                    print(" ", end="")
-                else:
-                    print(self.board[y][x], end="")
-            print("")
-    
-    def isValid(self, x, y):
-        return x >= 0 and x < self.width and y >= 0 and y < self.height
+# Constatnts, variables and objects
+width = 30
+height = 20
+difficulty = 10
 
-    def getLocal(self, x, y):
-        local = []
-        for yy in range(-1, 2):
-            for xx in range(-1, 2):
-                if self.isValid(x+xx, y+yy):
-                    local.append((self.board[y+yy][x+xx], (x+xx, y+yy)))
-        return local
-    
-    def checkLocal(self, x, y):
-        n = 0
-        for p, _ in self.getLocal(x, y):
-            if p == "*":
-                n += 1
-        return n
+r = roj.roj(width, height, difficulty)
 
-    def countBoard(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if not self.board[y][x] == "*":
-                    self.board[y][x] = str(self.checkLocal(x, y))
-    
-    def reveal(self, x, y, searched=[]):
-        for p, (xp, yp) in self.getLocal(x, y):
-            if (xp, yp) in searched:
-                pass
-            elif p == "0":
-                self.revealed[yp][xp] = True
-                searched.append((xp, yp))
-                self.reveal(xp, yp, searched)
-            elif p == "*":
-                pass
+#pygame.init()
+pygame.display.init()
+pygame.font.init()
+
+font = pygame.font.Font(pygame.font.get_default_font(), 25)
+display = pygame.display.set_mode((width*27,height*27))
+pygame.display.set_caption("Minesweeper")
+
+def drawBoard(boardstate):
+    for y in range(height):
+        for x in range(width):
+            if r.getRevealed(x, y):
+                col = (128, 128, 128)
             else:
-                self.revealed[yp][xp] = True
-    
-    
+                col = (255, 255, 255)
+            pygame.draw.rect(display, col, pygame.Rect(x*27, y*27, 25, 25))
+            if boardstate[y][x] != "0" and boardstate[y][x] != " " and boardstate[y][x] != "*":
+                display.blit(font.render(boardstate[y][x], True, (0, 0, 0)), (x*27+1, y*27+1))
+            elif boardstate[y][x] == "*":
+                display.blit(font.render("X", True, (0, 0, 0)), (x*27+1, y*27+1))
 
-r = roj(30, 20)
-r.showBoard(True)
-while True:
-    r.showBoard()
-    inp = input(">> ")
-    split = inp.split(",")
-    r.reveal(int(split[0]), int(split[1]))
 
+    pygame.display.flip()
+
+
+loop = True
+while loop:
+    drawBoard(r.getBoardstate())
+    pygame.event.pump()
+    if pygame.mouse.get_pressed()[0]:
+        (x, y) = pygame.mouse.get_pos()
+        if x >= 0 and x < width*27 and y >= 0 and y < height*27:
+            r.pick(int(x/27), int(y/27))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        loop = False
+
+pygame.font.quit()
+pygame.display.quit()
